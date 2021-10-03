92615e19dc58303e5f974092a85c3b3cf31e7dac
Kyohei Uto <kyoheiu@outlook.com>
2021-10-03 09:21:12 +0900

    Add rename_file

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,4 @@
+use super::entry::*;
 use std::fs;
 use std::path::PathBuf;
 use termion::{clear, cursor};
@@ -23,4 +24,16 @@ pub fn format_time(time: &Option<String>) -> String {
 
 pub fn clear_all() {
     print!("{}{}", clear::All, cursor::Goto(1, 1));
+}
+
+pub fn rename_file(mut name: String, entry_v: &Vec<EntryInfo>) -> String {
+    name.push_str("_copied");
+    if entry_v
+        .iter()
+        .any(|entry| entry.file_path.to_str().unwrap() == &name)
+    {
+        return rename_file(name, entry_v);
+    } else {
+        return name;
+    }
 }
