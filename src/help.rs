6377d66725ed60287722a76eee43dae713bfe763
kyoheiu <kyoheiu@outlook.com>
2022-04-12 21:53:18 +0900

    v0.6.0 / Fix appearance

--- src/help.rs
+++ src/help.rs
@@ -1,4 +1,4 @@
-pub const HELP: &str = "# felix v0.5.2
+pub const HELP: &str = "# felix v0.6.0
 A simple TUI file manager with vim-like keymapping.
 This program works on terminals with 21 columns or more.
 
@@ -10,28 +10,30 @@ Both relative and absolute available.
 `fx -h` or `fx --help` => Print help.
 
 ### Manual
-j / Key Up            :Go up.
-k / Key Down          :Go down.
-h / Key Left          :Go to parent directory if exists.
-l / Key Right / Enter :Open file or change directory.
-gg                    :Go to the top.
-G                     :Go to the bottom.
-dd                    :Delete and yank item.
-yy                    :Yank item.
-p                     :Put yanked item in the current directory.
-V                     :Switch to select mode.
-  - d                 :In select mode, delete and yank selected items.
-  - y                 :In select mode, yank selected items.
-t                     :Toggle sort order (name <-> modified time).
-backspace             :Toggle whether to show hidden items.
-:                     :Switch to shell mode.
-c                     :Switch to rename mode.
-/                     :Switch to filter mode.
-Esc                   :Return to normal mode.
-:e                    :Reload the current directory.
-:empty                :Empty the trash directory.
-:h                    :Show help.
-:q / ZZ               :Exit the program.
+j / Up            :Go up.
+k / Down          :Go down.
+h / Left          :Go to parent directory if exists.
+l / Right / Enter :Open file or change directory.
+gg                :Go to the top.
+G                 :Go to the bottom.
+dd                :Delete and yank item.
+yy                :Yank item.
+p                 :Put yanked item in the current directory.
+V                 :Switch to select mode.
+  - d             :In select mode, delete and yank selected items.
+  - y             :In select mode, yank selected items.
+t                 :Toggle sort order (name <-> modified time).
+backspace         :Toggle whether to show hidden items.
+:                 :Switch to shell mode.
+c                 :Switch to rename mode.
+/                 :Switch to filter mode.
+Esc               :Return to normal mode.
+:cd | :z          :Go to home directory.
+:z <keyword>      :*zoxide required* Jump to a directory that matches the keyword.
+:e                :Reload the current directory.
+:empty            :Empty the trash directory.
+:h                :Show help.
+:q / ZZ           :Exit the program.
 
 ## Configuration
 config file    : $XDG_CONFIG_HOME/felix/config.toml
