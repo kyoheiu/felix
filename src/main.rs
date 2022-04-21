4e18c8ad35c080e65385c36904bc1c632df2483b
kyoheiu <kyoheiu@outlook.com>
2022-04-22 06:31:54 +0900

    Add 'v' to clarify

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,9 @@ fn main() -> Result<(), errors::MyError> {
                             ver.chars().skip(9).take_while(|x| *x != '\"').collect();
                         if let Ok(current) = std::env::var("CARGO_PKG_VERSION") {
                             if latest != current {
-                                println!("felix {current}: Latest version is {latest}.");
+                                println!("felix v{current}: Latest version is {latest}.");
                             } else {
-                                println!("felix {current}: Up to date.");
+                                println!("felix v{current}: Up to date.");
                             }
                         }
                     }
