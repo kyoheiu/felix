6d1f1c8ec5501fb6f44503be23447d062a1d53f6
kyoheiu <kyoheiu@outlook.com>
2022-05-10 20:48:02 +0900

    State.new method returns result to handle terminal size error

--- src/state.rs
+++ src/state.rs
@@ -101,28 +101,75 @@ pub struct DeletedFiles {
     pub dir: PathBuf,
 }
 
-impl Default for State {
-    fn default() -> Self {
+macro_rules! print_item {
+    ($color: expr, $name: expr, $time: expr, $selected: expr, $layout: expr) => {
+        if $layout.terminal_column < PROPER_WIDTH {
+            if *($selected) {
+                print!("{}{}{}{}", $color, style::Invert, $name, style::Reset,);
+            } else {
+                print!("{}{}{}", $color, $name, color::Fg(color::Reset));
+            }
+            if $layout.terminal_column > $layout.time_start_pos + TIME_WIDTH {
+                print!("{}", clear::AfterCursor);
+            }
+        } else {
+            if *($selected) {
+                print!(
+                    "{}{}{}{}{}{} {}{}{}",
+                    $color,
+                    style::Invert,
+                    $name,
+                    style::Reset,
+                    cursor::Left(100),
+                    cursor::Right($layout.time_start_pos - 1),
+                    style::Invert,
+                    $time,
+                    style::Reset
+                );
+            } else {
+                print!(
+                    "{}{}{}{} {}{}",
+                    $color,
+                    $name,
+                    cursor::Left(100),
+                    cursor::Right($layout.time_start_pos - 1),
+                    $time,
+                    color::Fg(color::Reset)
+                );
+            }
+            if $layout.terminal_column > $layout.time_start_pos + TIME_WIDTH {
+                print!("{}", clear::AfterCursor);
+            }
+        }
+    };
+}
+
+impl State {
+    pub fn new() -> Result<Self, FxError> {
         let config = read_config().unwrap_or_else(|_| panic!("Something wrong with config file."));
         let session =
             read_session().unwrap_or_else(|_| panic!("Something wrong with session file."));
         let (column, row) =
             termion::terminal_size().unwrap_or_else(|_| panic!("Cannot detect terminal size."));
 
-        // Panic if terminal size is not enough to functionally work
-        if column < 21 {
+        // Return error if terminal size may cause panic
+        if column < 4 {
             log::error!("Too small terminal size.");
-            panic!("Panic due to terminal size (less than 21 columns).")
+            return Err(FxError::SmallSize {
+                msg: "Error: too small terminal size (less than 4 columns)".to_string(),
+            });
         };
         if row < 4 {
             log::error!("Too small terminal size.");
-            panic!("Panic due to terminal size (less than 4 rows).")
+            return Err(FxError::SmallSize {
+                msg: "Error: too small terminal size (less than 4 rows)".to_string(),
+            });
         };
 
         let (time_start, name_max) =
             make_layout(column, config.use_full_width, config.item_name_length);
 
-        State {
+        Ok(State {
             list: Vec::new(),
             registered: Vec::new(),
             manipulations: Manipulation {
