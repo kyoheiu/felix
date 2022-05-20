b8ad2f1faa2377fb5f8a69d30d063f6d72730def
kyoheiu <kyoheiu@outlook.com>
2022-05-20 16:34:17 +0900

    Implement some methods in Operation

--- src/op.rs
+++ src/op.rs
@@ -1,4 +1,5 @@
 use super::state::ItemInfo;
+use log::info;
 use std::path::PathBuf;
 
 #[derive(Debug, Clone)]
@@ -15,12 +16,6 @@ pub enum OpKind {
 }
 
 #[derive(Debug, Clone)]
-pub struct RenamedFile {
-    pub original_name: PathBuf,
-    pub new_name: PathBuf,
-}
-
-#[derive(Debug, Clone)]
 pub struct PutFiles {
     pub original: Vec<ItemInfo>,
     pub put: Vec<PathBuf>,
@@ -32,4 +27,69 @@ pub struct DeletedFiles {
     pub trash: Vec<PathBuf>,
     pub original: Vec<ItemInfo>,
     pub dir: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+pub struct RenamedFile {
+    pub original_name: PathBuf,
+    pub new_name: PathBuf,
+}
+
+impl Operation {
+    /// Discard undone operations when new one is pushed.
+    pub fn branch(&mut self) {
+        if self.count == 0 {
+            return;
+        }
+        for _i in 0..self.count {
+            self.op_list.pop();
+        }
+    }
+
+    pub fn push(&mut self, op: OpKind) {
+        log(&op);
+        self.op_list.push(op);
+        self.count = 0;
+    }
+}
+
+fn log(op: &OpKind) {
+    let mut result = String::new();
+    match op {
+        OpKind::Put(op) => {
+            result.push_str("PUT: ");
+            let put = path_to_string(&op.put);
+            result.push_str(&put);
+        }
+        OpKind::Delete(op) => {
+            result.push_str("DELETE: ");
+            let put = item_to_string(&op.original);
+            result.push_str(&put);
+        }
+        OpKind::Rename(op) => {
+            result.push_str("RENAME: ");
+            result.push_str(op.original_name.as_path().to_str().unwrap());
+            result.push_str(" -> ");
+            result.push_str(op.new_name.as_path().to_str().unwrap());
+        }
+    }
+    info!("{}", result);
+}
+
+fn item_to_string(v: &Vec<ItemInfo>) -> String {
+    let mut result = String::new();
+    for p in v {
+        result.push_str(&p.file_name);
+        result.push(' ');
+    }
+    result
+}
+
+fn path_to_string(v: &Vec<PathBuf>) -> String {
+    let mut result = String::new();
+    for p in v {
+        result.push_str(p.as_path().to_str().unwrap());
+        result.push(' ');
+    }
+    result
 }
