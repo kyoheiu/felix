9835f8964bd7a04e24efb2e069e80704851c1ed4
kyoheiu <kyoheiu@outlook.com>
2022-10-01 11:35:59 +0900

    Better error message with file path

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 #[derive(Debug)]
 pub enum FxError {
     Io,
@@ -6,10 +8,9 @@ pub enum FxError {
     TomlDe,
     TomlSer,
     WalkDir,
-    UTF8,
-    CopyItem,
-    RenameItem,
-    RemoveItem,
+    Encode,
+    PutItem(PathBuf),
+    RemoveItem(PathBuf),
     TooSmallWindowSize,
     Log,
 }
@@ -19,18 +20,17 @@ impl std::error::Error for FxError {}
 impl std::fmt::Display for FxError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let printable = match self {
-            FxError::Io => "Error: Io",
-            FxError::GetItem => "Error: Cannot get item info",
-            FxError::OpenItem => "Error: Cannot open item",
-            FxError::TomlDe => "Error: Cannot deserialize toml",
-            FxError::TomlSer => "Error: Cannot serialize toml",
-            FxError::WalkDir => "Error: Cannot read directory",
-            FxError::UTF8 => "Error: Incorrect encoding",
-            FxError::CopyItem => "Error: Cannot copy item",
-            FxError::RenameItem => "Error: Cannot rename item",
-            FxError::RemoveItem => "Error: Cannot remove item",
-            FxError::TooSmallWindowSize => "Error: Too small window size",
-            FxError::Log => "Error: Cannot initialize logger",
+            FxError::Io => "Error: Io".to_owned(),
+            FxError::GetItem => "Error: Cannot get item info".to_owned(),
+            FxError::OpenItem => "Error: Cannot open item".to_owned(),
+            FxError::TomlDe => "Error: Cannot deserialize toml".to_owned(),
+            FxError::TomlSer => "Error: Cannot serialize toml".to_owned(),
+            FxError::WalkDir => "Error: Cannot read directory".to_owned(),
+            FxError::Encode => "Error: Incorrect encoding".to_owned(),
+            FxError::PutItem(s) => format!("Error: Cannot copy -> {:?}", s),
+            FxError::RemoveItem(s) => format!("Error: Cannot remove -> {:?}", s),
+            FxError::TooSmallWindowSize => "Error: Too small window size".to_owned(),
+            FxError::Log => "Error: Cannot initialize logger".to_owned(),
         };
         write!(f, "{}", printable)
     }
